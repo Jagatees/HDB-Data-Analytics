3d4a9d623ee6225523d5e96073c1c3aa883e61f9
JagateesMac <140966272+Jagatees@users.noreply.github.com>
2023-09-06 21:25:35 +0800

    notes update

--- test4/scripts/your_script.py
+++ test4/scripts/your_script.py
@@ -1,16 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 # web scrapping
 def displayText():
-    url = "https://www.newegg.com/global/sg-en/msi-geforce-rtx-3060-rtx-3060-ventus-3x-12g-oc/p/N82E16814137631"
-    result = requests.get(url)
-    doc = BeautifulSoup(result.text, "html.parser")
-    # print(doc.prettify)
+    # url = "https://www.newegg.com/global/sg-en/msi-geforce-rtx-3060-rtx-3060-ventus-3x-12g-oc/p/N82E16814137631"
+    # result = requests.get(url)
+    # doc = BeautifulSoup(result.text, "html.parser")
+    # # print(doc.prettify)
 
-    prices = doc.find_all(text="$")
-    parent = prices[0].parent
-    strong = parent.find("strong")
-    print(strong.text)
+    # prices = doc.find_all(text="$")
+    # parent = prices[0].parent
+    # strong = parent.find("strong")
+    # print(strong.text)
+
+    # with open("./scripts/index.html", "r") as f:
+    #     doc = BeautifulSoup(f, "html.parser")
+
+    #     tag = doc.find_all(text=re.compile("\$.*"), limit=1)
+    #     for x in tag:
+    #         print(x.strip())
+
+    url = "https://coninmarketcap.com"
+    result = requests.get(url).text
+    doc = BeautifulSoup(result, "html.parser")
+
+      
 
 displayText()
