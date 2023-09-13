17827ca6ad56a182a5d94f70eab35d1457f8511d
Stars1999 <kerwinlohzy@gmail.com>
2023-09-13 19:55:14 +0800

    Prediction
    Graph not working

--- ProjTest/Main.py
+++ ProjTest/Main.py
@@ -160,8 +160,6 @@ def Analytics_Page():
         Predict_DF = Filter_DF.copy()
         print(Predict_DF)
 
-
-        #Not Working Yet
         Predicted_Data = Predict_DF[Predict_DF['Location'] == Y1parameters_combobox.get()]
 
         PredictX = Predicted_Data[['Year']]
@@ -175,8 +173,6 @@ def Analytics_Page():
         predicted_price = model.predict(Predict_2024)[0]
 
         print(predicted_price)
-
-
 
     #Take col name and store in dropdown 
     label4 = tk.Label(Analytics_frame, text="Y1 axis: ")
@@ -201,13 +197,115 @@ def Analytics_Page():
 
     Analytics_frame.pack(pady=20)
 
+
+
+
 #Function to show Data page
 def Data_Page():
     Data_frame = tk.Frame(main_frame)
 
     #Code here for Data page
-    lb = tk.Label(Data_frame, text='Data page', font=('Bold', 30))
-    lb.pack()
+    csv_file = 'ProjTest\Excel Data\TestRentalData.csv'
+    TestRentalCSV = pd.read_csv(csv_file, header=None)
+
+    #Clean data
+    Col_ToClean = [2]
+    for column in Col_ToClean:
+        TestRentalCSV[column] = TestRentalCSV[column].str.replace('$', '').str.replace(',', '')
+
+    #Take only selected col
+    TestRentalCSV = TestRentalCSV[[1, 2, 3, 4]]
+    #Change col name
+    TestRentalCSV.columns = ['Year', 'Price', 'Type' ,'Area']
+    #Drop first row
+    TestRentalCSV = TestRentalCSV.drop(0)
+    #Save dataframe into new csv file
+    TestRentalCSV.to_csv('ProjTest\Excel Data\Cleaned_Rent.csv', index=False)
+    #print(TestRentalCSV)
+    
+    #Get unique value from a col
+    AreaNames = TestRentalCSV['Area'].unique().tolist()
+    unique_Year = TestRentalCSV['Year'].unique().tolist()
+    #Filter_DF = pd.DataFrame()
+    #TestAreaName = []
+
+    def ShowGraph():
+        #Create empty list
+        TestAreaName = Y1parameters_combobox.get()
+
+        FilterResult_List = []
+        for year in unique_Year:
+            FilterTypeofRent = 'Room for Rent'
+            #YearRent = '2023'
+            FilterRentalCol = TestRentalCSV.copy()
+            FilterRentalCol = FilterRentalCol[FilterRentalCol['Year'].str.contains(year) & FilterRentalCol['Type'].str.contains(FilterTypeofRent) & FilterRentalCol['Area'].str.contains(TestAreaName) ]
+
+            # Check if the df is empty
+            if not FilterRentalCol.empty:
+                AverageRentList = list(FilterRentalCol['Price'])
+                RentList = [eval(x) for x in AverageRentList]
+                    
+                # Calculate the average rent
+                AverageRent = round(sum(RentList) / len(RentList), 2)
+                    
+                # Append the location and average rent to the FilterResult_List
+                FilterResult_List.append((year, TestAreaName, AverageRent))
+
+        #Store FilterResult_List in a dataframe
+        Filter_DF = pd.DataFrame(FilterResult_List, columns=['Year','Location','Average_Price'])
+        #print(Filter_DF)
+
+        #Data prediction
+        Predict_DF = Filter_DF.copy()
+        #print(Predict_DF)
+
+        Predicted_Data = Predict_DF[Predict_DF['Location'] == Y1parameters_combobox.get()]
+
+        PredictX = Predicted_Data[['Year']]
+        PredictY = Predicted_Data[['Average_Price']]
+
+        model = LinearRegression()
+
+        model.fit(PredictX, PredictY)
+
+        Predict_2024 = [[2024]]
+        predicted_price = model.predict(Predict_2024)[0]
+
+        Price_Predicted = float(predicted_price)
+        Xaxis = Predicted_Data['Year']
+        Yaxis = Predicted_Data['Average_Price']
+        print(Xaxis)
+
+        fig, ax = plt.subplots()
+
+        for years, Predicted_Data in Predict_DF.groupby('Year'):
+            ax.plot(Predicted_Data['Year'], Predicted_Data['Average_Price'], label=years)
+
+        #ax.plot(Xaxis, Yaxis, marker='o', label='Historical Data')
+
+        ax.plot(2024, Price_Predicted, marker='o', color='red', label=f'Predicted Price: {Price_Predicted:.2f}')
+        ax.set_xlabel('Year')
+        ax.set_ylabel('Average Price')
+        ax.set_title('Line Chart')
+        
+        canvas = FigureCanvasTkAgg(fig, Data_frame)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side="left", expand=False)
+        ax.legend()
+
+
+    #Take col name and store in dropdown 
+    label4 = tk.Label(Data_frame, text="Y1 axis: ")
+    Y1parameter_choices = AreaNames
+    selected_parameters = tk.StringVar()
+    Y1parameters_combobox = ttk.Combobox(Data_frame, textvariable=selected_parameters, values=Y1parameter_choices)
+    selected_parameters.set("Please select")
+
+    ShowGraphBtn = tk.Button(Data_frame, text="Show Graph", command=ShowGraph)
+
+    label4.pack(side='left') 
+    Y1parameters_combobox.pack(side='left', padx=5) 
+    ShowGraphBtn.pack()
 
     Data_frame.pack(pady=20)
 
