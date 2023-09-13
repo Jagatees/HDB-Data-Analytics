7cff350b922bae9e50ebb69bff5374079e359736
JagateesMac <140966272+Jagatees@users.noreply.github.com>
2023-09-13 19:19:01 +0800

    Can get html from website (Seleium)

--- test4/scripts/propertyguru/improvevScrapping.py
+++ test4/scripts/propertyguru/improvevScrapping.py
@@ -6,5 +6,24 @@ chrome_driver_path = '/Users/jagatees/Downloads/chromedriver'
 # Create a Chrome WebDriver instance
 driver = webdriver.Chrome()
 
-# Open YouTube
-driver.get('https://www.99.co/singapore/s/rent?bathrooms=any&building_age=any&composite_floor_level=any&composite_furnishing=any&composite_views=any&features_and_amenities=any&has_floor_plan=false&isFilterUnapplied=false&listing_type=rent&main_category=all&map_bounds=1.5827095153768858%2C103.49449749970108%2C1.1090706240313446%2C104.12483807587296&page_num=1&page_size=35&path=%2Fsingapore%2Frent&period_of_availability=any&property_segments=residential&query_coords=1.3039947%2C103.8298507&query_limit=radius&query_type=city&rental_type=all&rooms=any&show_cluster_preview=true&show_description=true&show_future_mrts=true&show_internal_linking=true&show_meta_description=true&show_nearby=true&zoom=11')
+# Open the URL
+url = 'https://www.99.co/singapore/s/rent?bathrooms=any&building_age=any&composite_floor_level=any&composite_furnishing=any&composite_views=any&features_and_amenities=any&has_floor_plan=false&isFilterUnapplied=false&listing_type=rent&main_category=all&map_bounds=1.5827095153768858%2C103.49449749970108%2C1.1090706240313446%2C104.12483807587296&page_num=1&page_size=35&path=%2Fsingapore%2Frent&period_of_availability=any&property_segments=residential&query_coords=1.3039947%2C103.8298507&query_limit=radius&query_type=city&rental_type=all&rooms=any&show_cluster_preview=true&show_description=true&show_future_mrts=true&show_internal_linking=true&show_meta_description=true&show_nearby=true&zoom=11'
+driver.get(url)
+
+# Wait for the page to load completely (you can adjust the timeout as needed)
+driver.implicitly_wait(10)  # Implicitly wait up to 10 seconds (adjust as needed)
+
+# Get the HTML content of the entire webpage
+html_content = driver.page_source
+
+# Close the WebDriver
+driver.quit()
+
+# Define the path for the HTML file
+html_file_path = 'output.html'
+
+# Write the entire HTML content to the file
+with open(html_file_path, 'w', encoding='utf-8') as html_file:
+    html_file.write(html_content)
+
+print(f"Entire HTML page has been saved to {html_file_path}")
