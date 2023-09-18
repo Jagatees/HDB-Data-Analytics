7bc4743ed118e4814689424a5e0722aa9e86d4bc
JagateesMac <140966272+Jagatees@users.noreply.github.com>
2023-09-18 18:38:17 +0800

    Scrap 99.co Main Page

---
+++ test4/scripts/99co/rename_folder.py
@@ -0,0 +1,27 @@
+import glob
+import os
+
+list_in_dir = []
+
+# rename all HTML page to simple name index.html
+def renameFiles(data_dir_path):
+    for count, filename in enumerate(os.listdir(data_dir_path)):
+        src = os.path.join(data_dir_path, filename)
+        dst = os.path.join(data_dir_path, f'{str(count)}.html')
+        os.rename(src, dst)
+
+
+# Return list of item in folder
+def get_item_in_dic(location):
+    txtfiles = []
+    arr = os.listdir(location)
+    for file in arr:
+        print(file)
+        txtfiles.append(file)
+    return txtfiles
+
+# renameFiles('page_scrape')
+print(get_item_in_dic())
+
+
+# get all name inside the directory of the folder and store into array
--- test4/scripts/99co/scrap_website.py
+++ test4/scripts/99co/scrap_website.py
@@ -1,3 +1,4 @@
+import os
 import time
 from bs4 import BeautifulSoup
 import json
@@ -12,83 +13,105 @@ link = []
 type_ = []
 num_beds = []
 num_toilet = []
+lease = []
+
+# Return list of item in folder
+def get_item_in_dic():
+    txtfiles = []
+    arr = os.listdir('page_scrape')
+    for file in arr:
+        txtfiles.append(file)
+    return txtfiles
 
 def test():
-    with open("page_scrape/page-1-0.html", "r") as f:
-        doc = BeautifulSoup(f, "html.parser")
 
-        # Get Higest Page Count 
-        page_count = doc.find(class_='kiAZx').find_all('a')[4]['aria-label'].split(' ')[1]
-        print(page_count)
+    list_item = get_item_in_dic()
+    print('Length : ' + str(len(list_item)))
 
-        title_of_room = doc.find_all(class_ = '_12dss')
-        x = 0
-        for index in title_of_room:
-            # Title 
-            title_of_room = index.find(class_ = '_3FkoX')
-            title_of_room = title_of_room.find('a')['title']
-            # print(title_of_room)
-            title.append(title_of_room)
+    counter = 0
 
-            # Price
-            price_of_room = index.find(class_ = '_3XjHl')
-            price_of_room = price_of_room.find_all('li')[1]['content'].split('/')[0][1:].replace(',','')
-            # print(price_of_room)
-            price.append(price_of_room)
+    for index in list_item:
+        print(index)
+        with open("page_scrape/" + str(index), "r") as f:
+            doc = BeautifulSoup(f, "html.parser")
 
-            # Link to Full Page
-            link_to_room = index.find(class_ = '_3FkoX')
-            link_to_room = 'https://www.99.co' + link_to_room.find('a')['href']
-            # print(link_to_room)
-            link.append(link_to_room)
+            # Get Higest Page Count 
+            page_count = doc.find(class_='kiAZx').find_all('a')[4]['aria-label'].split(' ')[1]
+            # print(page_count)
 
+            title_of_room = doc.find_all(class_ = '_12dss')
+            for index in title_of_room:
+                # Title 
+                title_of_room = index.find(class_ = '_3FkoX')
+                title_of_room = title_of_room.find('a')['title']
+                # print(title_of_room)
+                title.append(title_of_room)
 
-            # Type of Room
-            room_type = index.find('li', class_='_1LPAx', itemprop='accommodationCategory')
-            room_type = room_type.text
-            # print(room_type)
-            type_.append(room_type)
+                # Price
+                price_of_room = index.find(class_ = '_3XjHl')
+                price_of_room = price_of_room.find_all('li')[1]['content'].split('/')[0][1:].replace(',','')
+                # print(price_of_room)
+                price.append(price_of_room)
 
-             # X Beds
-            beds = index.find('li', class_='_1x-U1', itemprop='numberOfBedrooms')
-            if beds != None:
-                beds = beds.text.replace('\n','').replace(" ", "").replace("Beds", "")
-                # print(beds)
-                num_beds.append(beds)
-            else:
-                # print('None')
-                num_beds.append('None')
+                # Link to Full Page
+                link_to_room = index.find(class_ = '_3FkoX')
+                link_to_room = 'https://www.99.co' + link_to_room.find('a')['href']
+                # print(link_to_room)
+                link.append(link_to_room)
 
 
-            # X Beds
-            toilet = index.find('li', class_='_1x-U1', itemprop='numberOfBathroomsTotal')
-            if toilet != None:
-                toilet = toilet.text.replace('\n','').replace(" ", "").replace("Baths", "").replace("Bath", "")
-                # print(beds)
-                num_toilet.append(toilet)
-            else:
-                # print('None')
-                num_toilet.append('None')
+                # Type of Room
+                room_type = index.find('li', class_='_1LPAx', itemprop='accommodationCategory')
+                room_type = room_type.text
+                # print(room_type)
+                type_.append(room_type)
+
+                # X Beds
+                beds = index.find('li', class_='_1x-U1', itemprop='numberOfBedrooms')
+                if beds != None:
+                    beds = beds.text.replace('\n','').replace(" ", "").replace("Beds", "")
+                    # print(beds)
+                    num_beds.append(beds)
+                else:
+                    # print('None')
+                    num_beds.append('None')
 
 
-    
+                # X Beds
+                toilet = index.find('li', class_='_1x-U1', itemprop='numberOfBathroomsTotal')
+                if toilet != None:
+                    toilet = toilet.text.replace('\n','').replace(" ", "").replace("Baths", "").replace("Bath", "")
+                    # print(beds)
+                    num_toilet.append(toilet)
+                else:
+                    # print('None')
+                    num_toilet.append('None')
 
-            x = x + 1
 
+                 # Type of Room
+                le = index.find('li', itemprop='leaseLength')
+                if le != None:
+                    # print(le.text)
+                    lease.append(le.text)
+                else: 
+                    # print('None')
+                    lease.append('None')
+                
+                counter = counter + 1
 
 
     data = [
         {
-            'UID': i,  # Assign a UID based on the index 'i'
             'Title': title[i],
             'Price': price[i],
             'Links': link[i],
             'Type' : type_[i],
             'Num_Bed' : num_beds[i],
             'Num_Toilet' : num_toilet[i],
+            'Lease' : lease[i],
 
         }
-        for i in range(x)
+        for i in range(counter)
         
     ]
     # Save the data as JSON
