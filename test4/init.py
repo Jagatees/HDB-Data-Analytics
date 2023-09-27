c89605e4009c9f8946dbd4d225dd27f472c9468c
JagateesMac <140966272+Jagatees@users.noreply.github.com>
2023-09-27 14:40:58 +0800

    re change

--- test4/init.py
+++ test4/init.py
@@ -103,9 +103,8 @@ def get_options():
     return render_template('index.html', imagevalue=image_value)
 
 
-
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run('127.0.0.1', 5000)
 
 
 
