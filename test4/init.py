6c35411e686cc3888626a388c4bdc97f588ad5c3
JagateesMac <140966272+Jagatees@users.noreply.github.com>
2023-10-08 00:41:25 +0800

    Editing Map

--- test4/init.py
+++ test4/init.py
@@ -58,16 +58,21 @@ mapbox_styles = [
     Description : Hello World Test
     Methods : GET & POST
 '''
+
+
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     print('Welcome to ICT1001 Porgramming A1')
     return render_template('index.html')
+
 
 '''
     Route : /website
     Description : Get User Choice to chose which website to scrap
     Methods : GET & POST
 '''
+
+
 @app.route('/website', methods=['GET', 'POST'])
 def website():
     if request.method == 'POST':
@@ -77,13 +82,16 @@ def website():
         if selected_option == '1':
             return render_template('index.html', valueFive=co_firstpage.main())
         elif selected_option == '2':
-            return render_template('index.html', prop_value_page = srx_firstpage.main())
-        
+            return render_template('index.html', prop_value_page=srx_firstpage.main())
+
+
 '''
     Route : /get_page_count_co
     Description : Store Selected Options into a Global Var for 99co Website 
     Methods : POST
 '''
+
+
 @app.route('/get_page_count_co', methods=['POST'])
 def get_page_count_co():
     global user_input_page_count_co
@@ -92,18 +100,21 @@ def get_page_count_co():
     print(selected_option)  # Print the selected option for debugging
     return render_template('index.html')
 
+
 '''
     Route : /scrapping_co
     Description : Scrapping 99co Website 
     Methods : GET & POST
 '''
-@app.route('/scrapping_co', methods = ['GET', 'POST'])
+
+
+@app.route('/scrapping_co', methods=['GET', 'POST'])
 def scrapping_co():
     global user_input_page_count_co
     x = co_secondpage.main(int(user_input_page_count_co))
     co_thirdpage.renameFiles('centralized/99co/scrapping')
     co_fourpage.main('centralized/99co/scrapping')
-    return render_template('index.html', valueSix = x)
+    return render_template('index.html', valueSix=x)
 
 
 '''
@@ -111,6 +122,8 @@ def scrapping_co():
     Description : Store Selected Options into a Global Var for SRX Website 
     Methods : GET & POST
 '''
+
+
 @app.route('/get_page_count_prop', methods=['POST'])
 def get_page_count_prop():
     global user_input_page_count_prop
@@ -119,23 +132,100 @@ def get_page_count_prop():
     print(selected_option)  # Print the selected option for debugging
     return render_template('index.html')
 
+
 '''
     Route : /scrapping_prop
     Description : Scrapping SRX Website 
     Methods : GET & POST
 '''
-@app.route('/scrapping_prop', methods = ['GET', 'POST'])
+
+
+@app.route('/scrapping_prop', methods=['GET', 'POST'])
 def scrapping_prop():
     global user_input_page_count_prop
     x = srx_secondpage.main(int(user_input_page_count_prop))
     srx_thirdpage.main('centralized/srx/scrapping')
-    return render_template('index.html', prop_one = x)
+    return render_template('index.html', prop_one=x)
+
+
+'''
+    Route : /formatCO
+    Description : Clean & Format 99co JSON
+    Methods : GET & POST
+'''
+
+
+@app.route('/formatCO', methods=['GET', 'POST'])
+def formatCO():
+    if request.method == 'POST':
+        clean_CO.co_clean_data(
+            'scripts/merger_json/data.csv', 'scripts/merger_json/clean_data.csv')
+        return render_template('index.html')
+
+
+'''
+    Route : /merger_data
+    Description : Merger 99co & SRX Excel togather 
+    Methods : GET & POST
+'''
+
+
+@app.route('/merger_data', methods=['GET', 'POST'])
+def merger_data():
+    if request.method == 'POST':
+        return render_template('index.html')
+
+
+'''
+    Route : /request_chart
+    Description : Display Intrective Map from maplayout (folder)
+    Methods : GET & POST
+'''
+
+
+@app.route('/request_chart', methods=['GET', 'POST'])
+def request_chart():
+    if request.method == 'POST':
+        selected_option = request.form['my_dropdown_map']
+        plot_div = mapsone.generate_plotly_chart(
+            mapbox_styles[int(selected_option)])
+        return render_template('charts.html', plot_div=plot_div)
+
+
+'''
+    Route : /display_table
+    Description : Display Table from maplayout (folder)
+    Methods : GET & POST
+'''
+
+
+@app.route('/display_table', methods=['GET', 'POST'])
+def display_table():
+    if request.method == 'POST':
+        data = table_d.display_table()
+        return render_template('charts.html', data=data)
+
+
+'''
+    Route : /run_logic
+    Description : Run the Algo 
+    Methods : GET & POST
+'''
+
+
+@app.route('/run_logic', methods=['GET', 'POST'])
+def run_logic():
+    if request.method == 'POST':
+        alogone.algo()
+        return render_template('charts.html')
 
 
 '''
     TESTING 
 '''
-@app.route('/get_options', methods = ['GET', 'POST'])
+
+
+@app.route('/get_options', methods=['GET', 'POST'])
 def get_options():
     option_to_image = {
         '1': 'static/99co.png',
@@ -153,60 +243,14 @@ def get_options():
 def Charts():
     return render_template('charts.html')
 
+
 @app.route('/scrapping')
 def scrapping():
     return render_template('index.html')
 
 
-    
-# Json Cleaning 
-@app.route('/formatCO', methods = ['GET', 'POST'])
-def formatCO():
-    if request.method == 'POST':
-        clean_CO.co_clean_data('scripts/merger_json/data.csv','scripts/merger_json/clean_data.csv')
-        return render_template('index.html')
-    
-# Merge (PENDING FUNCTION)
-@app.route('/merger_data', methods = ['GET', 'POST'])
-def merger_data():
-    if request.method == 'POST':
-        return render_template('index.html')
-
-
-
-
-
-# Interactive Chart
-@app.route('/request_chart', methods = ['GET', 'POST'])
-def request_chart():
-    if request.method == 'POST':
-        selected_option = request.form['my_dropdown_map']
-        plot_div = mapsone.generate_plotly_chart(mapbox_styles[int(selected_option)])
-        return render_template('charts.html', plot_div=plot_div)
-    
-    
-
-# display_table of data 
-@app.route('/display_table', methods = ['GET', 'POST'])
-def display_table():
-    if request.method == 'POST':
-        data = table_d.display_table()
-        return render_template('charts.html', data = data)
-    
-# Logic
-@app.route('/run_logic', methods = ['GET', 'POST'])
-def run_logic():
-    if request.method == 'POST':
-        alogone.algo()
-        return render_template('charts.html')
-    
-    
 '''
     Run the Flask application on the local server 
 '''
 if __name__ == "__main__":
     app.run('127.0.0.1', 5005, debug=True)
-
-
-
-
--- test4/scripts/map_layout/chrolopleth_maps.py
+++ test4/scripts/map_layout/chrolopleth_maps.py
@@ -6,66 +6,71 @@ import pandas as pd
 import plotly.graph_objects as go
 
 
+def create_scattermapbox(file_path, trace_name):
+    df = pd.read_csv(file_path)
+    lat, lon = df['Lat'], df['Long']
+
+    scattermapbox_trace = go.Scattermapbox(
+        lat=lat,
+        lon=lon,
+        mode='markers',
+        marker=go.scattermapbox.Marker(
+            size=5,
+            color="#00ff00",
+        ),
+        text='None',  
+        name=trace_name,
+    )
+
+    return scattermapbox_trace
+
+
 def generate_plotly_chart(map_style):
-   
-    file_path1 = "scripts/algo/Excel/output/FilteredUserHse.csv"
-    file_path2 = "scripts/algo/Excel/Amenities/fairprice.csv"  # Replace with the path to your second CSV file
 
-    df1 = pd.read_csv(file_path1)
-    df2 = pd.read_csv(file_path2)
-
-    # Split and convert coordinates for the first DataFrame (if needed)
-    df1[['Latitude', 'Longitude']] = df1['Coordinates'].str.split(', ', expand=True)
-    df1['Latitude'] = pd.to_numeric(df1['Latitude'])
-    df1['Longitude'] = pd.to_numeric(df1['Longitude'])
-
-    # Extract latitude and longitude columns for the second DataFrame
-    lat2 = df2['Lat']
-    lon2 = df2['Long']
-
-    # Combine latitude and longitude columns from both DataFrames
-    latitude_column = pd.concat([df1['Latitude'], lat2])
-    longitude_column = pd.concat([df1['Longitude'], lon2])
-    
-    # Define different colors for df1 and df2
-    color1 = "#ff0000"  # Red for df1
-    color2 = "#00ff00"  # Green for df2
-
-    # Create a trace for df1
-    trace1 = go.Scattermapbox(
-        lat=df1['Latitude'],
-        lon=df1['Longitude'],
-        mode='markers',
-        marker=go.scattermapbox.Marker(
-            size=20,
-            color=color1,
-        ),
-        text=df1['Total_Points'],  # If df1 has "Total_Points"
-    )
-
-    # Create a trace for df2
-    trace2 = go.Scattermapbox(
-        lat=lat2,
-        lon=lon2,
-        mode='markers',
-        marker=go.scattermapbox.Marker(
-            size=20,
-            color=color2,
-        ),
-        text=None,  # No "Total_Points" in df2
-    )
-
-    # Set the desired center coordinates and zoom level
     center_lat = 1.3521  # Replace with your desired latitude
     center_lon = 103.8198  # Replace with your desired longitude
     zoom_level = 10  # Adjust the zoom level as needed
 
-    # Create a figure with both traces
-    fig = go.Figure(data=[trace1, trace2])
+    # for hdb is a special
+    hdb_file_path1 = "scripts/algo/Excel/output/FilteredUserHse.csv"
+    df1 = pd.read_csv(hdb_file_path1)
+    df1[['Latitude', 'Longitude']] = df1['Coordinates'].str.split(
+        ', ', expand=True)
+    df1['Latitude'] = pd.to_numeric(df1['Latitude'])
+    df1['Longitude'] = pd.to_numeric(df1['Longitude'])
 
-    fig.update_layout(mapbox_style=map_style, mapbox_center={"lat": center_lat, "lon": center_lon}, mapbox_zoom=zoom_level)
-    plot_div = fig.to_html(full_html=False) 
-    
+    # DF1
+    hdb_house_onsale = go.Scattermapbox(
+        lat=df1['Latitude'],
+        lon=df1['Longitude'],
+        mode='markers',
+        marker=go.scattermapbox.Marker(
+            size=15,
+            color="#ff0000",
+        ),
+        text= df1['Total_Points'],  # If df1 has "Total_Points"
+        name="HDB House",
+    )
+
+    # Replace with the path to your second CSV file
+    fairprice_file_path = "scripts/algo/Excel/Amenities/fairprice.csv"
+    hospital_file_path = "scripts/algo/Excel/Amenities/HospitalClinic.csv"
+
+    # Create a figure with both traces
+    fig = go.Figure(data=[
+        hdb_house_onsale, 
+        create_scattermapbox(fairprice_file_path, 'FairPrice'), 
+        create_scattermapbox(hospital_file_path, 'Hospital')])
+
+    fig.update_layout(mapbox_style=map_style, 
+                      mapbox_center={"lat": center_lat, "lon": center_lon}, 
+                      mapbox_zoom=zoom_level,
+                      height=720,
+                      width=980,
+                      title_text = "TITLE",
+                      title_x = 0.5,
+                      )
+    plot_div = fig.to_html(full_html=False)
 
     return plot_div
 
@@ -76,7 +81,7 @@ def generate_plotly_chart(map_style):
 #     new_df = pd.read_csv("scripts/algo/Excel/Amenities/fairprice.csv")
 #     new_lat = new_df['Lat']
 #     new_long = new_df['Long']
-    
+
 
 #     # Replace 'data.csv' with the actual file path to your CSV file.
 #     file_path = "scripts/algo/Excel/output/FilteredUserHse.csv"
@@ -98,16 +103,16 @@ def generate_plotly_chart(map_style):
 #     longitude_column = df['Longitude']
 
 #     num_points = len(df)
-  
+
 #     df['Amenties'] = np.random.randint(1, 100, num_points)
 #     df['size_dot'] = 1
 #     df['Description'] = "Plot1"
 
-   
-#     # work on the size problem 
+
+#     # work on the size problem
 #     # able to disable the data set and plot the map
 
-#     # House 
+#     # House
 #     fig = px.scatter_mapbox(df,
 #                             lon=longitude_column,
 #                             lat=latitude_column,
@@ -120,7 +125,6 @@ def generate_plotly_chart(map_style):
 #                             height=600,
 #                             opacity= 1
 #                             )
-    
 
 
 #     # num_points_new_df = len(new_df)
@@ -130,7 +134,7 @@ def generate_plotly_chart(map_style):
 
 #     # new_df['size_dot_2'] = fixed_marker_size
 #     # new_df['Description_2'] = "FairPrice"
-    
+
 
 #     # # Fairprice
 #     # fig.add_trace(px.scatter_mapbox(new_df,
@@ -147,7 +151,6 @@ def generate_plotly_chart(map_style):
 #     #          )
 #     fig.update_layout(mapbox_style=map_style)
 #     fig.update_layout(margin={"r": 0, "t": 50, "l": 0, "b": 10})
-
 
 
 #     # Convert Plotly chart to HTML
